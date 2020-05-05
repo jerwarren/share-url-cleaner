cda86f23943657e9a49443af0d7a3ca050b87f5d
Jer Warren <jerwarren@gmail.com>
2020-05-05 16:39:42 -0700

    re-share

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-function reShare(){
+function reShare(message){
     if (navigator.share) {
             navigator.share({
-            text: message,
+            url: message,
         })
         .then(() => console.log('Successful share'))
         .catch((error) => console.log('Error sharing', error));
     }
-}
-
-function displayShare(){
-
-    window.addEventListener('DOMContentLoaded', () => {
-        const parsedUrl = new URL(window.location);
-        // searchParams.get() will properly handle decoding the values.
-        console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
-        console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
-        console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
-      });
 }
